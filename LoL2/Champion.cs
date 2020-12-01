2e26c68868eb4292b33a1540640cb389b7a7d889
Toshihide-Sakao <toshihide.sakao@elev.ga.ntig.se>
2020-12-01 11:14:55 +0100

    burh

--- LoL2/Champion.cs
+++ LoL2/Champion.cs
@@ -15,11 +15,17 @@ namespace NovemberProjekt.LoL2
         public KeyboardKey d = KeyboardKey.KEY_D;
         public KeyboardKey f = KeyboardKey.KEY_F;
 
-        Vector2 mousePos = new Vector2(0, 0);
+        Vector2 moveMousePos = new Vector2(0, 0);
+        Vector2 qMousePos = new Vector2();
+        Vector2 wMousePos = new Vector2();
+        Vector2 eMousePos = new Vector2();
+        Vector2 rMousePOs = new Vector2();
 
         int counter = 1000000;
         int qLengthCounter = 1000000;
         int wLengthCounter = 1000000;
+
+        Vector2 qPos;
 
 
 
@@ -31,6 +37,8 @@ namespace NovemberProjekt.LoL2
             Position = spawnPos;
 
             moveSpeed = 2;
+
+            qPos = Position;
         }
 
         public override void Inputs()
@@ -47,17 +55,17 @@ namespace NovemberProjekt.LoL2
             if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_RIGHT_BUTTON))
             {
                 counter = 0;
-                mousePos = Raylib.GetMousePosition();
+                moveMousePos = Raylib.GetMousePosition();
             }
-            Moving(mousePos);
+            Moving(moveMousePos);
         }
 
         public void Cast()
         {
-            
             if (Raylib.IsKeyPressed(q))
             {
                 qLengthCounter = 0;
+                qMousePos = Raylib.GetMousePosition();
             }
             else if (Raylib.IsKeyPressed(w))
             {
@@ -65,19 +73,20 @@ namespace NovemberProjekt.LoL2
             }
             else if (Raylib.IsKeyPressed(e))
             {
-                
+
             }
             else if (Raylib.IsKeyPressed(r))
             {
-                
+
             }
-            qAbillity(mousePos);
+            qAbillity();
         }
 
-        public void qAbillity(Vector2 mousePos)
+        public void qAbillity()
         {
-            int qLength = 10;
+            int qLength = 100;
             int qSpeed = 5;
+<<<<<<< Updated upstream
             float ratio = CalcTotalMoveAmount(mousePos) / qLength;
             int forMoveAmount = (int)(CalcTotalMoveAmount(mousePos) * ratio) / qSpeed;
             Vector2 mSpeed = GetMoveSpeed(mousePos, forMoveAmount);
@@ -93,12 +102,35 @@ namespace NovemberProjekt.LoL2
             }
 
             
+=======
+            float ratio = qLength / CalcTotalMoveAmount(qMousePos); // calcing ratio between mousepos and targetpos
+            // int totalMoveAmount = (int)(CalcTotalMoveAmount(mousePos) * ratio); // calculating total amount to move to reach target. (diagonal)
+
+            int forMoveAmount = qLength / qSpeed; // amount to move every frame.
+
+            Console.WriteLine(qMousePos);
+            Vector2 bruh = new Vector2((qMousePos.X - Position.X) * ratio, (qMousePos.Y - Position.Y) * ratio);
+            // Vector2 targetPos = new Vector2(bruh.X * ratio, bruh.Y * ratio);
+            
+            Vector2 mSpeed = GetMoveSpeed(bruh, forMoveAmount);
+
+
+
+            if (qLengthCounter <= forMoveAmount)
+            {
+                qPos.X += mSpeed.X;
+                qPos.Y += mSpeed.Y;
+                qLengthCounter++;
+            }
+
+            Raylib.DrawCircle((int)qPos.X, (int)qPos.Y, 10, Color.RED);
+>>>>>>> Stashed changes
         }
 
         public void Moving(Vector2 mousePos)
         {
             int forMoveAmount = (int)CalcTotalMoveAmount(mousePos) / moveSpeed;
-            Vector2 mSpeed = GetMoveSpeed(mousePos, forMoveAmount);
+            Vector2 mSpeed = GetMoveSpeed(CalcMoveAmountXY(mousePos), forMoveAmount);
 
             if (counter <= forMoveAmount)
             {
@@ -107,35 +139,39 @@ namespace NovemberProjekt.LoL2
             }
         }
 
-        public Vector2 GetMoveSpeed(Vector2 mousePos, int forMoveAmount) 
+        public Vector2 GetMoveSpeed(Vector2 targetPos, int forMoveAmount)
         {
-            Vector2 moveAmount = CalcMoveAmountXY(mousePos);
             float mSpeedX = 0;
             float mSpeedY = 0;
 
-            if ((int)moveAmount.X != 0 && (int)forMoveAmount != 0)
+            if ((int)targetPos.X != 0 && (int)forMoveAmount != 0)
             {
-                mSpeedX = (float)moveAmount.X / forMoveAmount;
+                mSpeedX = (float)targetPos.X / forMoveAmount;
             }
-            if ((int)moveAmount.Y != 0 && (int)forMoveAmount != 0)
+            if ((int)targetPos.Y != 0 && (int)forMoveAmount != 0)
             {
-                mSpeedY = (float)moveAmount.Y / forMoveAmount;
+                mSpeedY = (float)targetPos.Y / forMoveAmount;
             }
 
             return new Vector2(mSpeedX, mSpeedY);
         }
 
-        public Vector2 CalcMoveAmountXY(Vector2 mousePos)
+        public Vector2 CalcMoveAmountXY(Vector2 allaj)
         {
-            return new Vector2(mousePos.X - Position.X, mousePos.Y - Position.Y);
+            return new Vector2(allaj.X - Position.X, allaj.Y - Position.Y);
         }
 
-        public float CalcTotalMoveAmount(Vector2 mousePos)
+        public float CalcTotalMoveAmount(Vector2 targetPos)
         {
-            Vector2 moveAmount = CalcMoveAmountXY(mousePos);
+            Vector2 moveAmount = CalcMoveAmountXY(targetPos);
             float totalMoveAmount = (float)Math.Sqrt((Math.Pow(moveAmount.X, 2) + Math.Pow(moveAmount.Y, 2)));
-            
+
             return totalMoveAmount;
+        }
+
+        public void bruuuuh(Vector2 targetPos, float totalMoveAmount, int speed)
+        {
+
         }
     }
 }
